efb26d53dab9b473943bd7b039de333159c98643
MohsenAghajani <aghajani69m@gmail.com>
2021-09-11 17:50:35 +0430

    add tags

---
+++ database/migrations/2021_09_09_091245_create_tag_post_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_post', function (Blueprint $table) {
+            $table->foreignIdFor(Tag::class);
+            $table->foreignIdFor(Post::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag_post');
+    }
+}
